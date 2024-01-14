d2eacbc456d1775aedffe7f9e58baa1bd3cfa832
foxrose <foxrose@kead.or.kr>
2024-01-14 21:21:23 +0900

    순서를 만들어 X 와 O를 번갈아 표시하기 승자 결정 하기   - 배열에 승리하는 모든 경우의 수의 인덱스를 작성하여   - for 문을 돌면서 첫번째 인덱스 값이 null 이 아니고     첫번 째 두번 째, 첫번 째 세 번째 값이 같은지 비교하여     모두 같으면 첫버 째 값을 반환하는 로직을 추가한다.   - 승자가 결정되거나, 이미 클릭한 위치에 다시 클릭하면 동작하지 않도록 프로그램을 수정 한다.

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,11 +4,50 @@ import './Board.css';
 
 const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setxIsNext] = useState(true);
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let index = 0; index < lines.length; index++) {
+      const [a, b, c] = lines[index];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        console.log('squares[a]', squares[a]);
+        return squares[a];
+      }
+    }
+    return null;
+  };
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = `Next player ${xIsNext ? 'X' : 'O'}`;
+  }
 
   const handleClick = (i) => {
     const newSquares = squares.slice();
-    newSquares[i] = 'X';
+
+    if (calculateWinner(newSquares) || newSquares[i]) {
+      return;
+    }
+    newSquares[i] = xIsNext ? 'X' : 'O';
     setSquares(newSquares);
+    // setxIsNext(!xIsNext);
+    setxIsNext((prev) => !prev);
   };
 
   const renderSquare = (i) => {
@@ -17,7 +56,7 @@ const Board = () => {
 
   return (
     <div>
-      <div className='status'>Next Player: X | O</div>
+      <div className='status'>{status}</div>
       <div className='board-row'>
         {renderSquare(0)}
         {renderSquare(1)}
