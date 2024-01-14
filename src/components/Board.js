9af627e99fabeb50087be61833a907892f695685
foxrose <foxrose@kead.or.kr>
2024-01-14 20:24:56 +0900

    class를 function으로 변경 - Board.js 파일에서   class Board를 const Board () => {} 로 변경 - Square.js 파일이세   class Square를 const Square = () => {} 로 변경   - props 는 const Square = (props) => {} 형태로 사용할 수도 있으나   - const Square = ({onClick, value}) => 를 사용하여 변경함

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,49 +1,40 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Sqluare from './Square';
 import './Board.css';
 
-export default class Board extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-    };
-  }
-  renderSquare(i) {
-    return (
-      <Sqluare
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
-      />
-    );
-  }
+const Board = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null));
 
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({ squares: squares });
-  }
+  const handleClick = (i) => {
+    const newSquares = squares.slice();
+    newSquares[i] = 'X';
+    setSquares(newSquares);
+  };
 
-  render() {
-    return (
-      <div>
-        <div className='status'>Next Player: X | O</div>
-        <div className='board-row'>
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className='board-row'>
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className='board-row'>
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+  const renderSquare = (i) => {
+    return <Sqluare value={squares[i]} onClick={() => handleClick(i)} />;
+  };
+
+  return (
+    <div>
+      <div className='status'>Next Player: X | O</div>
+      <div className='board-row'>
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
-    );
-  }
-}
+      <div className='board-row'>
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className='board-row'>
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+};
+
+export default Board;
