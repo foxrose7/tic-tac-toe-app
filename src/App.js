11aec360c0fb17cbabc8574e0fb23c64a95562d9
foxrose <foxrose@kead.or.kr>
2024-01-15 22:29:03 +0900

    tic-tac-toe 게임에 대한 history를 생성하기 위해서 squares의 복사본을 저장한다. history는 Board.에서도 사용하지만, 상태(history)에서도 사용하게 되므로 하위의 컴포넌트에서 공통으로 사용하는 경우 부모 컴포넌트에 위치시킨다는 규칙에 따라 App.js에 history 배열을 생성한다. - ES7+ React/Redux/React-Native snippets 플러그인을 설치 하였다면 - useState -> useStateSnippet를 선택하여 - const [history, setHistory] = useState([{squares: Array(9).fill(null)}]); 을 만드다.
    const [xIsNext, setxIsNext] ...
    const calculateWinne = () => ...
    const winner = calculateWinner(...를
    
    Board.js에서 잘라내어 App.js에 붙인다.
    
    빨간색으로 표시된 오류를 수정한다.
    
    Board.js 에서
    const Board = ({squares, onClick}) => {} 으로 수정하여 부모로 부터 props 를 받는다.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,66 @@
-import './App.css'
-import React from 'react'
-import Board from "./components/Board"
+import './App.css';
+import React from 'react';
+import { useState } from 'react';
+import Board from './components/Board';
 
 export default function App() {
+  const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
+  const [xIsNext, setxIsNext] = useState(true);
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let index = 0; index < lines.length; index++) {
+      const [a, b, c] = lines[index];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        console.log('squares[a]', squares[a]);
+        return squares[a];
+      }
+    }
+    return null;
+  };
+
+  const current = history[history.length - 1];
+  const winner = calculateWinner(current.squares);
+
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = `Next player ${xIsNext ? 'X' : 'O'}`;
+  }
+
+  const handleClick = (i) => {
+    const newSquares = current.squares.slice();
+    if (calculateWinner(newSquares) || newSquares[i]) {
+      return;
+    }
+
+    newSquares[i] = xIsNext ? 'X' : 'O';
+    setHistory([...history, { squares: newSquares }]);
+    setxIsNext((prev) => !prev);
+  };
+
   return (
-    <div className="game">
-      <div className="game-board"><Board/></div>
-      <div className="game-info">game-onfo</div>
+    <div className='game'>
+      <div className='game-board'>
+        <Board squares={current.squares} onClick={(i) => handleClick(i)} />
+      </div>
+      <div className='game-info'>
+        <div className='status'>{status}</div>
+      </div>
     </div>
-  )
+  );
 }
